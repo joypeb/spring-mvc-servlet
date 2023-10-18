3d50c423af5df457960d9fc9ba51dddceb027960
박은빈 <joypeb@naver.com>
2023-10-18 17:30:09 +0900

    first commit

---
+++ src/main/java/com/servlet/SpringMvcServletApplication.java
@@ -0,0 +1,13 @@
+package com.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcServletApplication.class, args);
+    }
+
+}
