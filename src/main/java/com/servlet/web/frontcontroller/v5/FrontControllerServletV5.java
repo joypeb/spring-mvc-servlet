67013b71256e098b6e03b6be4ea49a84c0b1daaf
박은빈 <joypeb@naver.com>
2023-10-24 16:42:13 +0900

    유연한 컨트롤러 v5 - v4추가

--- src/main/java/com/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/com/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -2,17 +2,19 @@ package com.servlet.web.frontcontroller.v5;
 
 import com.servlet.web.frontcontroller.ModelView;
 import com.servlet.web.frontcontroller.MyView;
-import com.servlet.web.frontcontroller.v3.ControllerV3;
 import com.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import com.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import com.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import com.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
+import com.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import com.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import com.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import com.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.servlet.HandlerAdapter;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,18 +29,23 @@ public class FrontControllerServletV5 extends HttpServlet {
     private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
 
     public FrontControllerServletV5() {
-        initHandlerMappinMap();
+        initHandlerMappingMap();
         initHandlerAdapters();
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
-    private void initHandlerMappinMap() {
+    private void initHandlerMappingMap() {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     @Override
